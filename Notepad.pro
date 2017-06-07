ce825b750e179c4b484fed6fb5822a3da2a92428
Emmad Arif <emmad.arif@yale.edu>
2017-06-07 22:37:00 +0500

    First commit

---
+++ Notepad.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-06-07T22:01:01
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Notepad
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        notepad.cpp
+
+HEADERS  += notepad.h
+
+FORMS    += notepad.ui
---
+++ notepad.cpp
@@ -0,0 +1,29 @@
+#include "notepad.h"
+#include "ui_notepad.h"
+
+Notepad::Notepad(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::Notepad)
+{
+    ui->setupUi(this);
+}
+
+Notepad::~Notepad()
+{
+    delete ui;
+}
+
+void Notepad::on_quitButton_clicked()
+{
+    QCoreApplication::quit();
+}
+
+void Notepad::on_actionOpen_triggered()
+{
+
+}
+
+void Notepad::on_actionSave_triggered()
+{
+
+}
